af4ef490b1a0278bae04da4d4bf461239bc6238f
Abdul Rehman <akabdulrehman159@gmail.com>
2025-07-21 17:49:58 +0500

    fix: remove extra components and unused code

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -87,9 +87,6 @@ export default function Dashboard() {
                 </CardHeader>
                 <CardContent>
                   <div className='text-2xl font-bold'>{doctors.length}</div>
-                  {/* <p className='text-muted-foreground text-xs'>
-                    +180.1% from last month
-                  </p> */}
                 </CardContent>
               </Card>
               <Card>
@@ -111,36 +108,8 @@ export default function Dashboard() {
                 </CardHeader>
                 <CardContent>
                   <div className='text-2xl font-bold'>{doctorSessions.length}</div>
-                  {/* <p className='text-muted-foreground text-xs'>
-                    +19% from last month
-                  </p> */}
                 </CardContent>
               </Card>
-              {/* <Card>
-                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                  <CardTitle className='text-sm font-medium'>
-                    Active Now
-                  </CardTitle>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    className='text-muted-foreground h-4 w-4'
-                  >
-                    <path d='M22 12h-4l-3 9L9 3l-3 9H2' />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className='text-2xl font-bold'>+573</div>
-                  <p className='text-muted-foreground text-xs'>
-                    +201 since last hour
-                  </p>
-                </CardContent>
-              </Card> */}
             </div>
             <div className='grid grid-cols-1 gap-4 lg:grid-cols-7'>
               <Card className='col-span-1 lg:col-span-4'>
